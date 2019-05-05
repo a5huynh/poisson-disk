fb9b4cc2686788c20dc7eac315f027a0935a2042
Andrew Huynh <a5thuynh@gmail.com>
2019-05-05 11:32:25 -0700

    Adding first pass at implementing the Bridson algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 mod utils;
 
+use js_sys::Math;
 use wasm_bindgen::prelude::*;
+
+const PI: f64 = 3.141529;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -9,11 +12,175 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    // No object at this location
+    EMPTY = 0,
+    // No longer on the active list
+    DEAD = 1,
+    // In the active list (used to generate additional points)
+    ACTIVE = 2
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, poisson-disk!");
+pub struct PoissonDisk {
+    width: u32,
+    height: u32,
+    cell_size: f64,
+    cell_width: f64,
+    cell_height: f64,
+    cells: Vec<Cell>,
+    radius: u32,
+    num_samples: usize,
+    // Grid used to determine point sampling.
+    grid: Vec<Option<(usize, usize)>>,
+    // List of points we want to generate more points around.
+    active: Vec<(usize, usize)>,
+}
+
+#[wasm_bindgen]
+impl PoissonDisk {
+    pub fn new(width: u32, height: u32, radius: u32) -> Self {
+        let cells = vec![Cell::EMPTY; (width * height) as usize];
+
+        // Step 0
+        // Initialize an n-dimensional background grid for storing samples
+
+        // We choose cell size to be radius / (dimensions) so that we
+        // are guaranteed to have at most one point in any given cell.
+        let cell_size = radius as f64 / (2.0 as f64).sqrt();
+        let cell_width = (width as f64 / cell_size).ceil() + 1.0;
+        let cell_height = (height as f64 / cell_size).ceil() + 1.0;
+        let grid = vec![None; (cell_width * cell_height) as usize];
+
+        let mut disk = PoissonDisk {
+            width,
+            height,
+            cells,
+            cell_size,
+            cell_width,
+            cell_height,
+            grid,
+            radius,
+            num_samples: 5,
+            active: Vec::new(),
+        };
+
+        // Step 1
+        // Select the initial sample to be randomly chosen uniformly in the domain.
+        let point = (
+            (Math::random() * width as f64) as usize,
+            (Math::random() * height as f64) as usize
+        );
+
+        disk.insert_point(point);
+        disk.active.push(point);
+
+        disk
+    }
+
+    fn distance(&self, pa: (usize, usize), pb: (usize, usize)) -> f64 {
+        let dx = pa.0 as f64 - pb.0 as f64;
+        let dy = pa.1 as f64 - pb.1 as f64;
+
+        (dx * dx + dy * dy).sqrt()
+    }
+
+    fn is_valid(&self, point: (usize, usize)) -> bool {
+        let xidx = (point.0 as f64 / self.cell_size).floor();
+        let yidx = (point.1 as f64 / self.cell_size).floor();
+
+        let start_x = (xidx - 2.0).max(0.0) as usize;
+        let end_x = (xidx + 2.0).min(self.cell_width - 1.0) as usize;
+        let start_y = (yidx - 2.0).max(0.0) as usize;
+        let end_y = (yidx + 2.0).min(self.cell_height - 1.0) as usize;
+
+        for x in start_x..end_x {
+            for y in start_y..end_y {
+                let cell_idx = y * self.cell_width as usize + x;
+                if let Some(grid_point) = self.grid[cell_idx] {
+                    if self.distance(point, grid_point) <= self.radius.into() {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        true
+    }
+
+    fn insert_point(&mut self, point: (usize, usize)) {
+        let cell_x = (point.0 as f64 / self.cell_size).floor();
+        let cell_y = (point.1 as f64 / self.cell_size).floor();
+
+        let idx = point.1 * self.width as usize + point.0;
+        self.cells[idx] = Cell::ACTIVE;
+
+        let cell_idx = (cell_y * self.cell_width + cell_x) as usize;
+        self.grid[cell_idx] = Some(point);
+    }
+
+    fn new_point(&mut self, point: (usize, usize)) -> (usize, usize) {
+        let theta = 2.0 * PI * Math::random();
+        // Pick a random radius between `r` and `2r`
+        let new_radius = self.radius as f64 * (Math::random() + 1.0);
+        // Find new coordinates relative to point p.
+        let new_x = point.0 as f64 + new_radius * theta.cos();
+        let new_y = point.1 as f64 + new_radius * theta.sin();
+
+        (
+            new_x.max(0.0).min(self.width as f64 - 1.0) as usize,
+            new_y.max(0.0).min(self.height as f64 - 1.0) as usize
+        )
+    }
+
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+
+    pub fn tick(&mut self) -> bool {
+        // While the active list is not empty, choose a random index.
+        if self.active.is_empty() {
+            return false
+        }
+
+        // Choose a point randomly from active list
+        let idx = (Math::random() * (self.active.len() - 1) as f64) as usize;
+        let point = self.active[idx];
+
+        // Generate up to `k` points chosen uniformly from the spherical
+        // annulus between radius `r` and `2r` around `x_{i}`.
+        let mut found = false;
+        for _ in 0..self.num_samples {
+            let new_point = self.new_point(point);
+            // Add the new point to the grid, active list, and to the
+            // final grid.
+            if self.is_valid(new_point) {
+                self.insert_point(new_point);
+                self.active.push(new_point);
+                found = true;
+            }
+        }
+
+        if !found {
+            self.active.remove(idx);
+            let cidx = point.1 * self.width as usize + point.0;
+            self.cells[cidx] = Cell::DEAD;
+        }
+
+        true
+    }
 }
--- www/index.js
+++ www/index.js
@@ -1,3 +1,88 @@
-import * as poisson from "poisson-disk";
+import { PoissonDisk } from "poisson-disk";
+import { memory } from "poisson-disk/poisson_disk_bg";
 
-poisson.greet();
+const CELL_SIZE = 5;
+const GRID_WIDTH = 256;
+const GRID_HEIGHT = 96;
+const RADIUS = 10;
+
+const disk = PoissonDisk.new(GRID_WIDTH, GRID_HEIGHT, RADIUS);
+
+const canvas = document.getElementById('viz');
+canvas.height = (CELL_SIZE + 1) * GRID_HEIGHT + 1;
+canvas.width = (CELL_SIZE + 1) * GRID_WIDTH + 1;
+
+const ctx = canvas.getContext('2d');
+
+const getIndex = (row, column) => {
+    return row * GRID_WIDTH + column;
+};
+
+const drawCells = () => {
+    const cellsPtr = disk.cells();
+    const cells = new Uint8Array(memory.buffer, cellsPtr, GRID_WIDTH * GRID_HEIGHT);
+
+    ctx.beginPath();
+
+    for (let row = 0; row < GRID_HEIGHT; row++) {
+        for (let col = 0; col < GRID_WIDTH; col++) {
+            const idx = getIndex(row, col);
+            if (cells[idx] === 1) {
+                ctx.fillStyle = '#000000';
+            } else if (cells[idx] === 2) {
+                ctx.fillStyle = '#FF0000';
+            } else {
+                ctx.fillStyle = '#FFFFFF';
+            }
+
+            ctx.fillRect(
+                col * (CELL_SIZE + 1) + 1,
+                row * (CELL_SIZE + 1) + 1,
+                CELL_SIZE,
+                CELL_SIZE
+            );
+        }
+    }
+
+    ctx.stroke();
+}
+
+const drawRadius = () => {
+    const cellsPtr = disk.cells();
+    const cells = new Uint8Array(memory.buffer, cellsPtr, GRID_WIDTH * GRID_HEIGHT);
+
+    for (let row = 0; row < GRID_HEIGHT; row++) {
+        for (let col = 0; col < GRID_WIDTH; col++) {
+            const idx = getIndex(row, col);
+            if (cells[idx] === 1 || cells[idx] === 2) {
+                ctx.beginPath();
+                let x = col * (CELL_SIZE + 1) + CELL_SIZE / 2 + 1;
+                let y = row * (CELL_SIZE + 1) + CELL_SIZE / 2 + 1;
+                ctx.ellipse(
+                    x, y,
+                    RADIUS, RADIUS,
+                    0, 0, 2 * Math.PI
+                );
+                ctx.ellipse(
+                    x, y,
+                    RADIUS * 2, RADIUS * 2,
+                    0, 0, 2 * Math.PI
+                );
+                ctx.stroke();
+            }
+        }
+    }
+}
+
+
+const renderLoop = () => {
+    drawCells();
+    drawRadius();
+    if (disk.tick()) {
+        requestAnimationFrame(renderLoop);
+    } else {
+        console.log('done!');
+    }
+};
+
+renderLoop();
